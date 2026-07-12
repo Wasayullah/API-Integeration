e04c2e87e0da464bf48957a63daaf63c3b40d5f9
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-07-12 15:05:18 +0000

    Add header to index and improve user data display styles

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 const getUser = document.getElementById("users")
 fetch('https://jsonplaceholder.typicode.com/users')
 .then(
-    (data)=>{
-        data.json
-    }
+    (data)=>data.json()
 )
-.then(
-    (user)=>{
-        user.forEach((users)=>{
-            
+.then((data) => {
+    data.forEach((user) => {
+       
         getUser.innerHTML+=`
         <div class='child'>
-            <p>Id: <span class="value">${users.id}</span></p>
-            <p>Name: <span class="value">${users.name}</span></p>
-            <p>Phone: <span class="value">${users.phone}</span></p>
-            <p>Email: <span class="value">${users.email}</span></p>
+            <p>Id: <span class="value">${user.id}</span></p>
+            <p>Name: <span class="value">${user.name}</span></p>
+            <p>Phone: <span class="value">${user.phone}</span></p>
+            <p>Email: <span class="value">${user.email}</span></p>
         </div>
         `
-    })
-}
-)
+    });
+})
 .catch(
     (err)=>{
         console.log(err)
