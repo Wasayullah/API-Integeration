1e8a85b496a6c858ee7ff11d1426fd99443db07b
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-07-12 14:48:15 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const getUser = document.getElementById("users")
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(
+    (data)=>{
+        data.json
+    }
+)
+.then(
+    (user)=>{
+        getUser.innerHTML+=`
+        <div class='child'>
+            <p>Id: <span class="value">${users.id}</span></p>
+            <p>Name: <span class="value">${users.name}</span></p>
+            <p>Phone: <span class="value">${users.phone}</span></p>
+            <p>Email: <span class="value">${users.email}</span></p>
+        </div>
+        `
+    }
+)
+.catch(
+    (err)=>{
+        console.log(err)
+    }
+)
